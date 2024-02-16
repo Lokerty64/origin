3b78f72715672c3d1379955443b8abb21d011b4d
Максим <146876826+Lokerty64@users.noreply.github.com>
2024-02-16 21:43:03 +0400

    Update isosTriangle.h

--- Lesson 5/Task 3/isosTriangle.h
+++ Lesson 5/Task 3/isosTriangle.h
@@ -1,6 +1,6 @@
 #pragma once
 
-class isosTriangle : public Triangle//đŕâíîáĺäđĺííűé ňđĺóăîëüíčę
+class isosTriangle : public Triangle
 {
 public:
 	isosTriangle(double a, double b, double A, double B);
