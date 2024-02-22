3927a030411eb13db06ba97a8077a2d780de7a71
Максим <146876826+Lokerty64@users.noreply.github.com>
2024-02-22 23:15:26 +0400

    Update Function.cpp

--- Lesson 6/Task 1/Function.cpp
+++ Lesson 6/Task 1/Function.cpp
@@ -5,7 +5,7 @@ int function(std::string anyWord, int forbiddenLength)
 {
     if (forbiddenLength == anyWord.length())
     {
-        throw std::exception("Âû ââåëè ñëîâî çàïðåòíîé äëèíû!Äî ñâèäàíèÿ.");
+        throw std::exception("Вы ввели слово запретной длины! До свидания");
     };
 
     return static_cast<int>(anyWord.length());
